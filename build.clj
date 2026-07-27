907b77de0dd089cccb698533b63a45a9863c3d11
yito88 <yito88@protonmail.com>
2026-07-27 22:42:24 +0200

    release workflow

---
+++ build.clj
@@ -0,0 +1,63 @@
+(ns build
+  "Builds and publishes the library jar.
+
+   The jar is `src` and nothing else: `examples/` and `test/` are on the
+   classpath only for their own aliases, so depending on jepsen-lite brings in
+   the library and not the demos.
+
+   `VERSION` comes from the environment -- the release workflow sets it from
+   the git tag -- so a local build is always a snapshot and can't be mistaken
+   for a release."
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'com.igel-data/jepsen-lite)
+(def version (or (System/getenv "VERSION") "0.0.0-SNAPSHOT"))
+(def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (clean nil)
+  (b/write-pom {:class-dir class-dir
+                :lib       lib
+                :version   version
+                :basis     @basis
+                :src-dirs  ["src"]
+                :pom-data [[:description
+                            (str "A lightweight, scoped-down fault-injection "
+                                 "and verification tool built on Jepsen's "
+                                 "internals")]
+                           [:url "https://github.com/igel-data/jepsen-lite"]
+                           [:licenses
+                            [:license
+                             [:name "Eclipse Public License 2.0"]
+                             [:url "https://www.eclipse.org/legal/epl-2.0/"]]]
+                           [:scm
+                            [:url "https://github.com/igel-data/jepsen-lite"]
+                            [:connection
+                             "scm:git:https://github.com/igel-data/jepsen-lite.git"]
+                            [:developerConnection
+                             "scm:git:ssh://git@github.com/igel-data/jepsen-lite.git"]
+                            [:tag (str "v" version)]]]})
+  (b/copy-dir {:src-dirs   ["src"]
+               :target-dir class-dir})
+  (b/copy-file {:src    "LICENSE"
+                :target (str class-dir "/META-INF/LICENSE")})
+  (b/jar {:class-dir class-dir
+          :jar-file  jar-file})
+  {:jar-file jar-file})
+
+(defn install [_]
+  (jar nil)
+  (dd/deploy {:installer :local
+              :artifact  (b/resolve-path jar-file)
+              :pom-file  (b/pom-path {:lib lib, :class-dir class-dir})}))
+
+(defn deploy [_]
+  (dd/deploy {:installer :remote
+              :artifact  (b/resolve-path jar-file)
+              :pom-file  (b/pom-path {:lib lib, :class-dir class-dir})}))
